6e00bf4b404c74aef4866db0b937d69d29f1c898
Geoffrey Caven <geoffrey@bravenew.world>
2016-12-23 14:57:15 -0400

    you can now shrink projectframes back to default state

--- app/scripts/components/ProjectFrame.js
+++ app/scripts/components/ProjectFrame.js
@@ -4,45 +4,49 @@ export default React.createClass({
   getInitialState: function() {
     return {expanded: false};
   },
-  toggleExpansion: function() {
+  expand: function() {
     if (!this.props.otherPaneExpanded) {
-      this.setState({expanded: !this.state.expanded});
+      this.setState({expanded: true});
       this.props.toggleTop();
     }
   },
+  shrink: function() {
+    this.setState({expanded: false});
+    this.props.toggleTop();
+  },
 	render: function() {
-		return (
-      <div>
-        { this.state.expanded ?
-          <div className={'projectFrame fullHeight ' + this.props.project.cssClass}>
-            <div className={this.props.project.cssClass + '-title projectTitle'}>{this.props.project.title}</div>
-            <div
-              className="glyphicon glyphicon-remove-circle"
-              onClick={this.toggleExpansion}
-            />
-            <div className="imagePane"></div>
-            <div className={this.props.project.cssClass + '-body bodyCopy'}>
-              <h3 className="subtitle">{this.props.project.subtitle}</h3>
-              {this.props.project.body}
-              <a href={this.props.project.url} target="_blank" className="projLink">Check It Out<span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span></a>
-            </div>
-          </div>
-        :
+    if (this.state.expanded) {
+      return (
+        <div className={'projectFrame fullHeight ' + this.props.project.cssClass}>
+          <div className={this.props.project.cssClass + '-title projectTitle'}>{this.props.project.title}</div>
           <div
-            className={'projectFrame ' + this.props.project.cssClass}
-            onClick={this.toggleExpansion}
-          >
-            <div className={this.props.project.cssClass + '-title projectTitle'}>{this.props.project.title}</div>
-            <div className="glyphicon glyphicon-remove-circle"></div>
-            <div className="imagePane"></div>
-            <div className={this.props.project.cssClass + '-body bodyCopy'}>
-              <h3 className="subtitle">{this.props.project.subtitle}</h3>
-              {this.props.project.body}
-              <a href={this.props.project.url} target="_blank" className="projLink">Check It Out<span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span></a>
-            </div>
+            className="glyphicon glyphicon-remove-circle"
+            onClick={this.shrink}
+          />
+          <div className="imagePane"></div>
+          <div className={this.props.project.cssClass + '-body bodyCopy'}>
+            <h3 className="subtitle">{this.props.project.subtitle}</h3>
+            {this.props.project.body}
+            <a href={this.props.project.url} target="_blank" className="projLink">Check It Out<span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span></a>
           </div>
-        }
-      </div>
-    )
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className={'projectFrame ' + this.props.project.cssClass}
+          onClick={this.expand}
+        >
+          <div className={this.props.project.cssClass + '-title projectTitle'}>{this.props.project.title}</div>
+          <div className="glyphicon glyphicon-remove-circle"></div>
+          <div className="imagePane"></div>
+          <div className={this.props.project.cssClass + '-body bodyCopy'}>
+            <h3 className="subtitle">{this.props.project.subtitle}</h3>
+            {this.props.project.body}
+            <a href={this.props.project.url} target="_blank" className="projLink">Check It Out<span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span></a>
+          </div>
+        </div>
+      );
+    }
   }
 });
